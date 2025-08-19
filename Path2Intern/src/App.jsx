4b5663b05fb8f4535841df28317ef96f659a404e
it23736832@my.sliit.lk <himasha221d@gmail.com>
2025-08-19 11:57:33 +0530

    changed again

--- Path2Intern/src/App.jsx
+++ Path2Intern/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <p> hello this is himasha</p>
+      <p>this is new</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
